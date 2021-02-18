969346bf1cc316cbb36622507f86bba45550eb16
edijs1984 <ziedins.edijs@gmail.com>
2021-02-19 00:10:11 +0200

    some changes

--- src/apiLinks/httpUsers.js
+++ src/apiLinks/httpUsers.js
@@ -1,13 +1,13 @@
 //login
-export const loginApi = "/api/auth/login";
-export const loginConfimrApi = "/api/auth/confirm";
+export const loginApi = "/user/api/auth/login";
+export const loginConfimrApi = "/user/api/auth/confirm";
 
-export const userCreateApi = "/api/user/create";
+export const userCreateApi = "/user/api/user/create";
 export const userDeleteApi = "/api/user/delete";
-export const userEditApi = "/api/user/edit";
+export const userEditApi = "/user/api/user/edit";
 
 export const userAllApi = "/api/user/query/allusers";
 export const userFindOneApi = "/api/user/query/findone";
 
-export const userChangePasswordApi = "/api/user/password/change";
-export const userResetPasswordApi = "/api/user/password/reset";
+export const userChangePasswordApi = "/user/api/user/password/change";
+export const userResetPasswordApi = "/user/api/user/password/reset";
